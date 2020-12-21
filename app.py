8dccbd37429fbbccb0d684e5e4a758708b057782
felixdasilva <76109537+felixdasilva@users.noreply.github.com>
2020-12-21 14:30:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
+
 # Core Pkgs
 import streamlit as st 
 
 # EDA Pkgs
-import pandas as pd
+import pandas as pd 
 import codecs
-#import pandas_profiling import ProfileReport
-import pandas_profiling as pp
+from pandas_profiling import ProfileReport 
 
 # Components Pkgs
 import streamlit.components.v1 as components
-import streamlit_pandas_profiling as spp
-#from streamlit_pandas_profiling import st_profile_report
+from streamlit_pandas_profiling import st_profile_report
 
-st.set_page_config(page_title="Data Quality Profling Tool",page_icon='📈',layout="wide", initial_sidebar_state="expanded")
+#Page Setup
+st.set_page_config(page_title="Data Quality Profling Tool",page_icon='ðŸ“ˆ',layout="wide", initial_sidebar_state="expanded")
 st.sidebar.header("Read Me")
 st.sidebar.header("Help")
 st.sidebar.text_area("Need help? Let us know.")
@@ -31,21 +31,21 @@ def main():
     st.subheader("Sample Data from File")
     st.dataframe(df.head())
     st.subheader("Data Quality Profile")
-    profile = pp.ProfileReport(df, minimal=True)
-    #filepath = st.text_input("Where do you want to save the report?")
-    #if filepath is not None:
-    #  download = st.button("Download Report")
-   #   if download:
-   #     profile.to_file(filepath+"\Data Quality Profile.html")   
-    spp.st_profile_report(profile)
+    profile = ProfileReport(df, minimal=True)
+    filepath = st.text_input("Where do you want to save the report?")
+    if filepath is not None:
+      download = st.button("Download Report")
+      if download:
+        profile.to_file(filepath+"\Data Quality Profile.html")   
+    st_profile_report(profile)
         
 
 if __name__ == '__main__':
   main()
-  #hide_streamlit_style = """
- # <style>
-  ##MainMenu {visibility: hidden;}
- #footer {visibility: hidden;}
-  #  </style>
- #"""
- # st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+  hide_streamlit_style = """
+  <style>
+  #MainMenu {visibility: hidden;}
+  footer {visibility: hidden;}
+    </style>
+  """
+  st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
