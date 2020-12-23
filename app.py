1d63369b1d6521f139db3edb6fc06d286423acff
felixdasilva <76109537+felixdasilva@users.noreply.github.com>
2020-12-23 13:53:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def get_binary_file_downloader_html(bin_file, file_label='File'):
     with open(bin_file, 'rb') as f:
         data = f.read()
     bin_str = base64.b64encode(data).decode()
-    href = f'<h3><a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">>>>Download {file_label}<<<</a></h3>'
+    href = f'<h3><a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">***Download {file_label}***</a></h3>'
     return href
         
 
