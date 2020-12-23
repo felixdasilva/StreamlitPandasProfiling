b0e4354fcc180d532d1b9be21da7cd85f53fd4c8
felixdasilva <76109537+felixdasilva@users.noreply.github.com>
2020-12-23 12:46:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -41,11 +41,11 @@ def main():
 #Download File Command
 import os
 import base64
-def get_binary_file_downloader_html(bin_file, file_label='File'):
+def get_binary_file_downloader_html(bin_file, file_label='Data Quality Profile Report'):
     with open(bin_file, 'rb') as f:
         data = f.read()
     bin_str = base64.b64encode(data).decode()
-    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}"><h3>Download {file_label}</h3></a>'
     return href
         
 
